f51233bab5c41facfa2b6f8e4d0015b64a0daf29
Lornamwangi <149935850+Lornamwangi@users.noreply.github.com>
2024-01-12 15:36:42 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -99,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
